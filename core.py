80e4dc1cb4644a85cdd5b93efd178bc3507dc41c
Nikita <v.beats@inbox.ru>
2023-12-03 06:14:59 +0300

    update

--- core.py
+++ core.py
@@ -17,20 +17,10 @@ class SpamerSettings:
                     if hasattr(self, key):
                         setattr(self, key, value)
         else:
-            print(f"File not found: {file_path}")
+            with open(file_path, "w", encoding="utf-8") as file:
+                file.write("phone=\napi_id=\napi_hash=\ntimeout=\n")
 
     def check_all_fields_filled(self):
         all_fields_filled = all([getattr(self, field) for field in vars(self)])
         return all_fields_filled
 
-if __name__ == '__main__':
-    # Укажите путь к файлу с настройками, если он не в том же каталоге, где и скрипт
-    script_directory = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(script_directory, 'settings.txt')
-    
-    settings = SpamerSettings(file_path)
-
-    if settings.check_all_fields_filled():
-        print("All fields are filled.")
-    else:
-        print("Some fields are not filled. Please check your settings.")
